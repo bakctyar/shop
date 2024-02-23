1a431ccb7c4719a26618d857dd11eec3d680cee5
backtyar <b11703597@gmail.com>
2024-02-23 15:36:41 +0600

    first_commit

---
+++ cart/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, product_id)
+    form = CartAddProductForm(request.POST)
